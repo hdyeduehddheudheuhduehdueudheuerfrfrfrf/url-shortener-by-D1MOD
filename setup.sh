c04d1cf37be5d6598817114c003698f28b5e99d2
D1MOD1877 <99124744+mrxD1MOD@users.noreply.github.com>
2022-02-17 16:58:06 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+banner() {
+clear
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m  _    _ _____  _          _____ _    _  ____  _____ _______ ______ _   _ ______ _____   \e[0m\n"
+printf "\e[0m\e[1;93m | |  | |  __ \| |        / ____| |  | |/ __ \|  __ \__   __|  ____| \ | |  ____|  __ \  \e[0m\n"
+printf "\e[0m\e[1;93m | |  | | |__) | |  _____| (___ | |__| | |  | | |__) | | |  | |__  |  \| | |__  | |__) | \e[0m\n"
+printf "\e[0m\e[1;93m | |  | |  _  /| | |______\___ \|  __  | |  | |  _  /  | |  |  __| | . ` |  __| |  _  /  \e[0m\n"
+printf "\e[0m\e[1;93m | |__| | | \ \| |____    ____) | |  | | |__| | | \ \  | |  | |____| |\  | |____| | \ \  \e[0m\n"
+printf "\e[0m\e[1;93m \____/|_|  \_\______|  |_____/|_|  |_|\____/|_|  \_\ |_|  |______|_| \_|______|_|  \_\  \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m        CODED AND CREATED BY D1MOD1877 \e[0m\e[1;91m( \e[0m\e[1;92mhttps://discord.gg/sRWZxq9v \e[0m\e[1;91m)\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    Website :\e[0m\e[1;96m https://www.d1modev.ml/\e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m @d1mod1877\e[0m\n"
+}
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installing Packages..\e[0m\n"
+printf "\e[0m\n"
+apt update
+apt install curl -y
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Creating Environment...\e[0m\n"
+printf "\e[0m\n"
+cp -f url /data/data/com.termux/files/usr/bin/shorturl
+cp -f url /data/data/com.termux/files/usr/bin/ShortUrl
+echo 'termux-open-url https://github.com/htr-tech/' > /data/data/com.termux/files/usr/bin/htr-tech
+echo 'termux-open-url https://github.com/htr-tech/' > /data/data/com.termux/files/usr/bin/HTR-TECH
+echo 'termux-open-url https://www.instagram.com/tahmid.rayat/' > /data/data/com.termux/files/usr/bin/instagram
+echo 'termux-open-url https://www.instagram.com/tahmid.rayat/' > /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/htr-tech
+chmod +x /data/data/com.termux/files/usr/bin/HTR-TECH
+chmod +x /data/data/com.termux/files/usr/bin/instagram
+chmod +x /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/shorturl
+chmod +x /data/data/com.termux/files/usr/bin/ShortUrl
+sleep 2
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installation Succeed !!\e[0m\n"
+sleep 2
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Type \e[0m\e[1;92mShortUrl \e[0m\e[1;93mto Run this Tool \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\n"
